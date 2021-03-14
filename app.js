ec101ff0b74b63607c68a7b09235ba748ce33954
Saqib Suleman <saqibs3291@gmail.com>
2021-03-14 19:07:21 +0500

    add complete logic

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ date.innerHTML = new Date().getFullYear();
 
 //close link
 const navToggle = document.querySelector(".nav-toggle");
-const linkContainer = document.querySelector(".links-container");
+const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", () => {
-  const containerHeight = linksContainer.getBoundingClientRect().height;
   const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
 
   if (containerHeight === 0) {
-    linkContainer.style.height = `${linksHeight}px`;
+    linksContainer.style.height = `${linksHeight}px`;
   } else {
-    linkContainer.style.height = 0;
+    linksContainer.style.height = 0;
   }
 });
 
@@ -42,4 +42,33 @@ window.addEventListener("scroll", () => {
   } else {
     topLink.classList.remove("show-link");
   }
+});
+
+//select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    //navigate to specific post
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //calculate heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
 });
