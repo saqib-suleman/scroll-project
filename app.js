a4ed82a44f30b6f75505a05440b11ab3e0a86b2b
Saqib Suleman <saqibs3291@gmail.com>
2021-03-14 17:59:20 +0500

    add basic logic

--- app.js
+++ app.js
@@ -2,3 +2,44 @@
 // pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
+
+// setting year in footer
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+//close link
+const navToggle = document.querySelector(".nav-toggle");
+const linkContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linkContainer.style.height = `${linksHeight}px`;
+  } else {
+    linkContainer.style.height = 0;
+  }
+});
+
+//fixed navbar
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
